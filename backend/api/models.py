295e0d4df297034473fb10c9a27270fbf84092a4
angeloabianchi <angeloabianchi@gmail.com>
2023-10-27 10:45:31 +0200

    django react integration and api app created

---
+++ backend/api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class DandaProjects(models.Model):
+    name = models.CharField(max_length=200)
+    video = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+class ProjectPhotos(models.Model):
+    project = models.ForeignKey(DandaProjects, on_delete=models.CASCADE)
+    url = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.url
