2ea3affeaab5830ba61411f373599cba4011ae3c
angeloabianchi <angeloabianchi@gmail.com>
2023-11-24 14:29:34 +0100

    updated text on about page

--- backend/frontend/src/Pages/About/About.js
+++ backend/frontend/src/Pages/About/About.js
@@ -14,37 +14,63 @@ const About = () => {
           </div>
           <div className="col-lg-6 col-md-12 mb-5 dandaAboutText textOne">
             <p>
-              Nacido en Barcelona en 1987, Daniel es un apasionado director y
+              {/* Nacido en Barcelona en 1987, Daniel es un apasionado director y
               productor español que se graduó en cinematografía en la
               prestigiosa ESCAC (Escuela de Cine y Audiovisuales de Cataluña).
               Tras obtener su título, embarcó en una emocionante travesía en la
-              industria cinematográfica.
+              industria cinematográfica. */}
+              Nacido en Barcelona en 1987, es un director y productor español de
+              ascedencia francesa. Se graduó en cinematografía en la ESCAC
+              (Escuela de Cine y Audiovisuales de Cataluña) y poco después se
+              instaló en Los Ángeles donde estuvo rodando videoclips, anuncios y
+              documentales. Cuando volvió a Barcelona, su amplia visión del
+              sector y su personalidad inquieta le han llevado a transitar entre
+              la publicidad, el documental el cine y la televisión.
             </p>
             <p>
-              Su búsqueda de conocimiento lo llevó a Los Ángeles, donde sumergió
+              {/* Su búsqueda de conocimiento lo llevó a Los Ángeles, donde sumergió
               sus habilidades en la dirección de anuncios y videoclips,
               descubriendo una perspectiva más holística del proceso
               cinematográfico. Esta experiencia internacional marcó un punto de
               inflexión en su carrera, proporcionándole una visión única y
-              enriquecedora.
+              enriquecedora. */}
+              Después de trabajar para las principales productoras catalanas
+              entra como jefe de vídeo en Playground, una agencia de publicidad
+              y medio digital líder en España. Ahí puede desarrollar al máximo
+              su creatividad dedicándose a innovadores proyectos audiovisuales,
+              generalmente relacionados con temas sociales documentales y
+              proyectos publicitarios de lo más diversos; pudiendo trabajar para
+              su propia ciudad, dirigiendo las campañas publicitarias del
+              Ayuntamiento de Barcelona durante dos años.
             </p>
           </div>
           <div className="col-lg-6 col-md-12 dandaAboutText textTwo">
             <p>
-              Su búsqueda de conocimiento lo llevó a Los Ángeles, donde sumergió
+              {/* Su búsqueda de conocimiento lo llevó a Los Ángeles, donde sumergió
               sus habilidades en la dirección de anuncios y videoclips,
               descubriendo una perspectiva más holística del proceso
               cinematográfico. Esta experiencia internacional marcó un punto de
               inflexión en su carrera, proporcionándole una visión única y
-              enriquecedora.
+              enriquecedora. */}
+              Ha trabajado en documentales y programas de televisión adquiridos
+              por TVE, TV3, Flooxer, Spotify y Amazon Prime Video.
             </p>
             <p>
-              Dirigiendo campañas para el Ayuntamiento de Barcelona durante dos
+              {/* Dirigiendo campañas para el Ayuntamiento de Barcelona durante dos
               años, Daniel aportó su creatividad a proyectos publicitarios
               diversos, colaborando con marcas nacionales e internacionales como
               AXA, Levis, Lidl, San Pellegrino, Tesla, Amazon, El Pavo, Danone,
               y muchas más. Su excepcional trabajo fue reconocido con un
-              prestigioso premio Laus en la categoría de dirección.
+              prestigioso premio Laus en la categoría de dirección. */}
+              En publicidad ha trabajado para marcas nacionales e
+              internacionales como AXA, Levis, Lidl, San Pellegrino, Tesla,
+              Amazon, El Pavo, Danone, Panrico, Moritz y Playz. Destacando un
+              premio Laus como director.
+            </p>
+            <p>
+              Actualmente está en fase de desarrollo con A3 Media para dirigir
+              su primer largometraje de ficción basado en la novela Viaje al
+              País de los Blancos. La película está confirmada.
             </p>
           </div>
           <div className="col-lg-6 col-md-12 mb-5 imageTwo">
@@ -55,20 +81,22 @@ const About = () => {
           </div>
           <div className="col-lg-6 col-md-12 dandaAboutText textThree">
             <p>
-              Daniel ha dejado su marca en el ámbito documental, con obras
-              adquiridas por TVE, TV3, Flooxer, Spotify y Amazon Prime Video. Su
-              enfoque en proyectos relacionados con temas sociales demuestra su
-              compromiso con la narración visual que impacta y resuena con la
-              audiencia.
-            </p>
-            <p>
-              Actualmente, Daniel es asociado de la productora Atomic Beat,
+              {/* Actualmente, Daniel es asociado de la productora Atomic Beat,
               donde se embarca en nuevos desafíos. Participa activamente en la
               creación de programas para TV3 y en el desarrollo de contenido
               para televisión y plataformas. Entre sus proyectos actuales se
               encuentran "Movidos por el deporte," una serie documental, y
               "Dreamland," un cautivador largometraje que aborda la inmigración
-              africana.
+              africana. */}
+              Dani es una persona entregada al máximo en todo lo que hace. Ama
+              esta profesión y todo lo que conlleva. Es sensible, enérgico y
+              creativo. Es un apasionado del mundo que lo rodea y de su gente.
+              Ha viajado a más de 45 países, amante del deporte extremo y de la
+              naturaleza. Todo esto hace que le encante vincularse a proyectos
+              creativos que aporten algo más a la sociedad. Su background, su
+              sensibilidad y su amplio conocimiento del sector audiovisual le
+              hacen capaz de liderar cualquier producción como director o
+              productor ejecutivo.
             </p>
           </div>
         </div>
