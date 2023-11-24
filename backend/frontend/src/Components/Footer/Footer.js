eec9f732896aa67c95bd227046d9d6a533ff02b8
angeloabianchi <angeloabianchi@gmail.com>
2023-11-24 17:00:59 +0100

    footer updated and contact page started

--- backend/frontend/src/Components/Footer/Footer.js
+++ backend/frontend/src/Components/Footer/Footer.js
@@ -2,41 +2,21 @@ import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
 import DandaLogo from "../../static/img/DandaLogo2_500x500.png";
-import linkedin from "../../static/img/linkedin.png";
-import vimeo from "../../static/img/vimeo.png";
-import email from "../../static/img/email.png";
-import navBarBackground from "../../static/video/navbarBackground.mp4";
+
 
 const Footer = () => {
   const date = new Date();
   let year = date.getFullYear();
   return (
     <div className="FooterContainer">
-      {/* <div className="videoFooter">
-        <video className="video" preload="true" autoPlay loop muted>
-          <source src={navBarBackground} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div> */}
       <div className="footerContent">
+        <div className="footerLeft w-100">
+          <p>&#169;{year}-DANI </p>
+        </div>
         <div className="footerRight w-100">
           <Link to={"/"} className="logoLinkFooter">
             <img className="logoImageFooter" src={DandaLogo} alt="Danda Logo" />
           </Link>
-        </div>
-        <div className="footerCenter w-100">
-          <p>&#169;{year}-DANI </p>
-        </div>
-        <div className="footerLeft w-100">
-          <a className="col-4" href="#">
-            <img src={linkedin} tag="Linkedin Icon" />
-          </a>
-          <a className="col-4" href="#">
-            <img src={vimeo} tag="Vimeo Icon" />
-          </a>
-          <a className="col-4" href="#">
-            <img src={email} tag="Email Icon" />
-          </a>
         </div>
       </div>
     </div>
--- backend/frontend/src/Pages/Contacts/Contacts.js
+++ backend/frontend/src/Pages/Contacts/Contacts.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Contact.css";
+import vimeo from "../../static/img/vimeo.png";
+import instagram from "../../static/img/instagram.png";
 
 const Contacts = () => {
-  return <div className="contactPage">Contact</div>;
+  return (
+    <div className="contactPage">
+      <div className="contact">
+        <h4>EMAIL</h4>
+        <h1 className="mb-5">danisancho87@gmail.com</h1>
+        <h4>PHONE</h4>
+        <h1 className="mb-5">+34 647 282 554</h1>
+        <h4>SOCIAL MEDIAS</h4>
+        <div className="socialMediaSection">
+          <a className="d-flex justify-content-center" href="#">
+            <img src={instagram} tag="instagram Icon" className="socialMediaIcons"/>
+          </a>
+          <a className="d-flex justify-content-center" href="#">
+            <img src={vimeo} tag="Vimeo Icon" className="socialMediaIcons"/>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+
 };
 
 export default Contacts;
